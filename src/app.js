f484d75b805166502a4afd91e2e22d5d07552370
Iago Amaro <iagoamaro@hotmail.com>
2019-06-11 19:00:00 -0300

    Criando o projeto

---
+++ src/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+//Carrega Rotas
+const index = require('./routes/index');
+const product = require('./routes/product');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', index);
+app.use('/products', product);
+
+
+module.exports = app;
