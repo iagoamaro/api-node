b9d43774a81ede8b0544bb3cf5329fc61204ac36
Iago Amaro <iagoamaro@hotmail.com>
2019-06-12 17:32:04 -0300

    Add controller e mongo

--- src/app.js
+++ src/app.js
@@ -1,20 +1,35 @@
 'use strict'
 
+//Requires 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+//
 
+//App conf
 const app = express();
 const router = express.Router();
+//
+
+//Connection string
+mongoose.connect('mongodb+srv://teste:teste@cluster0-9npqy.mongodb.net/nodestr', { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+//
+
+//Carrega as Models
+const Product = require('./models/product');
 
 //Carrega Rotas
 const index = require('./routes/index');
 const product = require('./routes/product');
+//
 
+//App run
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use('/', index);
 app.use('/products', product);
+//
 
 
 module.exports = app;
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,29 +2,15 @@
 
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/products-controller');
 
+//Rotas
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
+router.post('/', controller.post);
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
-router.get('/', (req, res, nest) => {
-    res.status(200).send({
-        title: "Node API",
-        version: "0.0.1"
-    });
-});
-
-router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-router.put('/:id', (req, res, next) => {
-    const id = req.param.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
 
 module.exports = router;
